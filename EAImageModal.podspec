fd4957da97464fb71ad9e48f2a9a6e132925c279
Erick Arroyo <erick.1726@gmail.com>
2018-04-03 02:25:24 -0500

    iOS 8 support added

--- EAImageModal.podspec
+++ EAImageModal.podspec
@@ -16,7 +16,7 @@ Pod::Spec.new do |s|
   #
 
   s.name         = "EAImageModal"
-  s.version      = "0.1.2"
+  s.version      = "1.0.0"
   s.summary      = "An easy to use modal that displays an image"
 
   # This description is used to generate tags and improve search results.
@@ -63,7 +63,7 @@ Pod::Spec.new do |s|
   #
 
   # s.platform     = :ios
-  s.platform     = :ios, "9.0"
+  s.platform     = :ios, "8.0"
 
   #  When using multiple platforms
   # s.ios.deployment_target = "5.0"
--- EAImageModal/ImageModalView.swift
+++ EAImageModal/ImageModalView.swift
@@ -29,61 +29,42 @@ class ImageModalView: UIView {
         self.callbackOnClose = cb;
     }
     
-    public func setUpView(){
-        //let bundle = Bundle(for: type(of: self))
+    private func setUpView() {
+
         self.contentView = UIView()
-        self.contentView.backgroundColor = UIColor.black.withAlphaComponent(0.7)
         
-        
+        self.contentView.backgroundColor = UIColor.black.withAlphaComponent(0.8)
         addSubview(contentView)
-        
-        
-        
-        
         contentView.translatesAutoresizingMaskIntoConstraints = false
-        contentView.topAnchor.constraint(equalTo: self.topAnchor, constant: 0).isActive = true
-        contentView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: 0).isActive = true
-        contentView.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 0).isActive = true
-        contentView.rightAnchor.constraint(equalTo: self.rightAnchor, constant: 0).isActive = true
-        
-        
-        
-        
-        
+        self.addConstraint( NSLayoutConstraint(item: contentView, attribute: .top, relatedBy: .equal, toItem: self, attribute: .top, multiplier: 1, constant: 0))
+        self.addConstraint( NSLayoutConstraint(item: contentView, attribute: .bottom, relatedBy: .equal, toItem: self, attribute: .bottom, multiplier: 1, constant: 0))
+        self.addConstraint( NSLayoutConstraint(item: contentView, attribute: .left, relatedBy: .equal, toItem: self, attribute: .left, multiplier: 1, constant: 0))
+        self.addConstraint( NSLayoutConstraint(item: contentView, attribute: .right, relatedBy: .equal, toItem: self, attribute: .right, multiplier: 1, constant: 0))
         
         // Image
         self.imageModal  = UIImageView();
         self.contentView.addSubview(self.imageModal)
-        
         self.imageModal.translatesAutoresizingMaskIntoConstraints = false
-        self.imageModal.centerXAnchor.constraint(equalTo: self.contentView.centerXAnchor).isActive = true
-        
-        self.imageModal.topAnchor.constraint(equalTo: self.contentView.topAnchor, constant: 61).isActive = true
-        self.imageModal.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor, constant: -60).isActive = true
-        self.imageModal.widthAnchor.constraint(equalToConstant: 200).isActive = true
-        self.imageModal.heightAnchor.constraint(equalToConstant: 200).isActive = true
-        self.imageModal.contentMode = UIViewContentMode.scaleAspectFill
-        
-        
-        
-        
+        self.contentView.addConstraint(NSLayoutConstraint(item: self.imageModal, attribute: NSLayoutAttribute.centerX, relatedBy: .equal, toItem: self.contentView, attribute: NSLayoutAttribute.centerX, multiplier: 1, constant: 0))
+        self.contentView.addConstraint(NSLayoutConstraint(item: self.imageModal, attribute: .top, relatedBy: .equal, toItem: self.contentView, attribute: .top, multiplier: 1, constant: 61))
+        self.contentView.addConstraint(NSLayoutConstraint(item: self.imageModal, attribute: .bottom, relatedBy: .equal, toItem: self.contentView, attribute: .bottom, multiplier: 1, constant: -60))
+        self.contentView.addConstraint(NSLayoutConstraint(item: self.imageModal, attribute: .right, relatedBy: .equal, toItem: self.contentView, attribute: .right, multiplier: 1, constant: -20))
+        self.contentView.addConstraint(NSLayoutConstraint(item: self.imageModal, attribute: .left, relatedBy: .equal, toItem: self.contentView, attribute: .left, multiplier: 1, constant: 20))
+        self.imageModal.contentMode = UIViewContentMode.scaleAspectFit
+
         // Button
         self.closeButton = UIButton(type: .custom)
         self.contentView.addSubview(self.closeButton)
         self.closeButton.addTarget(self, action: #selector(removeSelf), for: .touchUpInside)
         self.closeButton.translatesAutoresizingMaskIntoConstraints = false
-        self.closeButton.topAnchor.constraint(equalTo: self.contentView.topAnchor, constant: 20).isActive = true
-        self.closeButton.rightAnchor.constraint(equalTo: self.contentView.rightAnchor, constant: -20).isActive = true
-        self.closeButton.widthAnchor.constraint(equalToConstant: 24).isActive = true
-        self.closeButton.heightAnchor.constraint(equalToConstant: 24).isActive = true
-        //self.closeButton.setImage(UIImage(named: "ic_close_modal", in: bundle, compatibleWith: nil), for: .normal)
+        self.contentView.addConstraint(NSLayoutConstraint(item: self.closeButton, attribute: .top, relatedBy: .equal, toItem: self.contentView, attribute: .top, multiplier: 1, constant :20))
+        self.contentView.addConstraint(NSLayoutConstraint(item: self.closeButton, attribute: .right, relatedBy: .equal, toItem: self.contentView, attribute: .right, multiplier: 1, constant :-20))
+        self.closeButton.addConstraint(NSLayoutConstraint(item: self.closeButton, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 24))
+        self.closeButton.addConstraint(NSLayoutConstraint(item: self.closeButton, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 24))
         self.closeButton.setImage(UIImage(named: "ic_image_modal_close_button"), for: .normal)
         contentView.alpha = 0.0
-        
     }
-    
-
-    
+       
     public override func didMoveToSuperview() {
         self.contentView.transform = CGAffineTransform(scaleX: 0.5, y: 0.5)
         UIView.animate(withDuration: 0.15, animations: {
